dc49757d5b90dd018a50291c1128a2d0aaaaece2
juon <juonjang@gmail.com>
2024-07-24 15:45:40 +0700

    edit register

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -14,6 +14,7 @@ import { createTheme, ThemeProvider } from '@mui/material/styles';
 import MenuItem from '@mui/material/MenuItem';
 import Snackbar from '@mui/material/Snackbar';
 import MuiAlert, { AlertProps } from '@mui/material/Alert';
+import { blueGrey, deepPurple } from '@mui/material/colors';
 
 const theme = createTheme();
 
@@ -92,19 +93,17 @@ export default function Register() {
         <CssBaseline />
         <Box
           sx={{
-            marginTop: 8,
+            marginTop: 10,
             display: 'flex',
             flexDirection: 'column',
             alignItems: 'center',
           }}
         >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-            {/* โลโก้ */}
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Register a new account
+          <Avatar sx={{ bgcolor: deepPurple[500] }}>NB</Avatar>
+          <Typography component="h1" variant="h5" sx={{ color: '#004d40' ,fontFamily:'kanit' , margin : 2}} >
+          กายภาพบำบัด โรงพบาลหนองบัวลำภู 
           </Typography>
-          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }} >
             <Grid container spacing={2}>
               <Grid item xs={12} sm={6}>
                 <TextField
